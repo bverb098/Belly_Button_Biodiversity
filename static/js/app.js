ae37f04e495dc5cba4d12884a3ad5255bfc1e099
bverb098 <102575708+bverb098@users.noreply.github.com>
2022-08-21 21:49:49 +1000

    finished

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,6 @@
 const dataPath = "../../samples.json";
 
+//update demographic info card
 function updateDemographicInfo(demographicInfo, subjectDropdown) {
    var currentSubjectId = subjectDropdown.property("value")
    console.log("current subject: ", currentSubjectId)
@@ -14,8 +15,9 @@ function updateDemographicInfo(demographicInfo, subjectDropdown) {
       var itemText = `${key}: ${value}`
       var listItem = infoCard.select("ul").append("li").text(itemText) 
    });
-}
+};
 
+//update bar chart
 function updateBarChart(chartData, subjectDropdown) {
    var currentSubjectId = subjectDropdown.property("value")
    var currentSubjectSample = chartData.filter(sample => {
@@ -48,6 +50,7 @@ function updateBarChart(chartData, subjectDropdown) {
    Plotly.newPlot("barChart",traceData,layout)
 };
 
+// update bubble chart
 function updateBubbleChart (chartData, subjectDropdown) {
    var currentSubjectId = subjectDropdown.property("value")
    var currentSubjectSample = chartData.filter(sample => {
@@ -66,7 +69,8 @@ function updateBubbleChart (chartData, subjectDropdown) {
       mode: 'markers',
       marker: {
          size: yValues,
-         // color: xValues
+         color: xValues,
+         colorscale: "YlGnBu"
       }
    };
    var layout = {
@@ -78,6 +82,8 @@ function updateBubbleChart (chartData, subjectDropdown) {
    Plotly.newPlot("bubbleChart",traceData,layout)
 };
 
+
+// update gauge
 function updateGuage (demographicInfo,subjectDropdown) {
    var currentSubjectId = subjectDropdown.property("value")
    var subjectInfo = demographicInfo.filter(sample=> {
@@ -86,46 +92,44 @@ function updateGuage (demographicInfo,subjectDropdown) {
    var guageValue = subjectInfo[0].wfreq
    console.log("wash frequency: ", guageValue)
 
-   var trace1 = {
+   var data = [
+      {
       type: "indicator",
-      mode:"guage+number",
+      mode: "gauge+number",
       value: guageValue,
-      title: {
-         align: "center",
-         text: "Belly Button Washing Frequency"
-      },
-      guage: {
-         axis: {range:[null,9], tickwidth:1},
-         bar: { color: "darkblue" },
-         shape: "angular",
+      title: { text: "Belly Button Washing Frequency", font: { size: 24 } },
+      gauge: {
+         axis: { range: [null, 9], showticklabels:true, nticks:10},
+         bar: {thickness:0.5, color:"blue"},
+         bgcolor: "white",
+         borderwidth: 2,
+         bordercolor: "gray",
          steps: [
-            {range: [1-2], color:"rgb(250, 250, 110)"},
-            {range: [2-3], color:"rgb(181, 232, 119)"},
-            {range: [3-4], color:"rgb(119, 209, 131)"},
-            {range: [4-5], color:"rgb(63, 183, 141)"},
-            {range: [5-6], color:"rgb(0, 156, 143)"},
-            {range: [6-7], color:"rgb(0, 127, 134)"},
-            {range: [7-8], color:"rgb(28, 99, 115)"},
-            {range: [8-9], color:"rgb(42, 72, 88)"}
-   
-         ]
+            {range: [0,1], color:"rgb(249, 203, 203)",line:{width:1}},
+            {range: [1,2], color:"rgb(255, 201, 190)",line:{width:1}},
+            {range: [2,3], color:"rgb(255, 202, 175)",line:{width:1}},
+            {range: [3,4], color:"rgb(255, 205, 158)",line:{width:1}},
+            {range: [4,5], color:"rgb(253, 210, 143)",line:{width:1}},
+            {range: [5,6], color:"rgb(238, 217, 131)",line:{width:1}},
+            {range: [6,7], color:"rgb(216, 224, 125)",line:{width:1}},
+            {range: [7,8], color:"rgb(186, 232, 128)",line:{width:1}},
+            {range: [8,9], color:"rgb(147, 240, 140)",line:{width:1}}
+         ],
       },
-   }
-
-   var layout = {
-      width: 500,
-      height: 400,
-      margin: { t: 25, r: 25, l: 25, b: 25 },
-      paper_bgcolor: "lavender",
-      font: { color: "darkblue", family: "Arial" }
-    };
-
-   guageData=[trace1]
-
-   Plotly.newPlot("washGuage",guageData,layout)
-
+      }
+   ];
+ 
+ var layout = {
+   // width: 400,
+   // height: 300,
+   paper_bgcolor: "white",
+   font: { color: "black", family: "Arial" }
+ };
+ 
+ Plotly.newPlot('washGuage', data, layout);
 };
 
+// call fall the functions to create the dashboard
 function initDash(subjectIds, demographicInfo, chartData, subjectDropdown) {
 
    // call the updateDemographicInfo function to create the demographic info card 
@@ -141,7 +145,7 @@ function initDash(subjectIds, demographicInfo, chartData, subjectDropdown) {
    updateGuage(demographicInfo, subjectDropdown)
 };
 
-
+// read the json, create the dropdown menu, set d3 events, call function to create dash
 d3.json(dataPath).then((samples) => {
    //create variables containing data from json needed for different parts of the dash
    const subjectIds = samples.names
@@ -158,6 +162,7 @@ d3.json(dataPath).then((samples) => {
       var subjectOption = subjectDropdown.append("option").text(name)
    });
 
+   
    // call the initDash function to create the dashboard when landing on the page
    initDash(subjectIds, demographicInfo, chartData, subjectDropdown)
 
@@ -165,5 +170,5 @@ d3.json(dataPath).then((samples) => {
    subjectDropdown.on("change", function() {
       initDash(subjectIds, demographicInfo, chartData, subjectDropdown)
    });
-  
 });
+
